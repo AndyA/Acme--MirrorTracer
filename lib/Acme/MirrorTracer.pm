bf92a4f13055ce7b76d1b73021df0785322b933b
Andy Armstrong <andy@hexten.net>
2009-05-16 21:12:11 +0000

    Bump.

--- lib/Acme/MirrorTracer.pm
+++ lib/Acme/MirrorTracer.pm
@@ -9,11 +9,11 @@ Acme::MirrorTracer - Do nothing.
 
 =head1 VERSION
 
-This document describes Acme::MirrorTracer version 2009.0516141813
+This document describes Acme::MirrorTracer version 2009.0516221139
 
 =cut
 
-our $VERSION = '2009.0516141813';
+our $VERSION = '2009.0516221139';
 
 =head1 DESCRIPTION
 
